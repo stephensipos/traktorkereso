24e08659de9851ba0a93b46232d2e99c915048f0
root <git@goabela.com>
2020-11-16 15:14:47 +0100

    frontend: base react app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  console.log("hello")
+  return (
+    <div className="App">
+      Traktorkereső
+    </div>
+  );
+}
+
+export default App;
